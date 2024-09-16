e288a30bb473a3e606931b6107c304ab1f001ed4
elizabethvilloria <lizvilloria@gmail.com>
2024-09-17 01:21:23 +0800

    Add basic password generator UI

---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>Password Generator</h1>
+      <input type="text" placeholder="Generated password" readOnly />
+      <button>Generate Password</button>
+    </div>
+  );
+}
+
+export default App;
