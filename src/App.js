10c38ecfd00991c0255ca6c5063010362931eb6c
elizabethvilloria <lizvilloria@gmail.com>
2024-09-17 01:24:35 +0800

    Add comments to explain password generation logic

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { useState } from 'react';
 function App() {
   const [password, setPassword] = useState('');
 
+  // Function to generate a random password
   const generatePassword = () => {
     const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*';
     let newPassword = '';
@@ -15,7 +16,9 @@ function App() {
   return (
     <div className="App">
       <h1>Password Generator</h1>
+      {/* Input to display the generated password */}
       <input type="text" value={password} readOnly />
+      {/* Button to trigger password generation */}
       <button onClick={generatePassword}>Generate Password</button>
     </div>
   );
